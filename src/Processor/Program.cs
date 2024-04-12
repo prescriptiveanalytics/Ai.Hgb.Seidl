54f801022daaa9a301194b3a04162cb64e645c9e
Jan Zenisek <janzenisek@gmx.at>
2024-04-12 12:45:04 +0200

    updated sample descriptions

--- src/Processor/Program.cs
+++ src/Processor/Program.cs
@@ -10,12 +10,13 @@ namespace Ai.Hgb.Seidl.Processor // Note: actual namespace depends on the projec
     //public static string demoTextFilePath = @"../../../../Samples/Lmd.3l";
     //public static string demoTextFilePath = @"../../../../Samples/resinet_democombined.3l";
     //public static string demoTextFilePath = @"../../../../Samples/resinet_instantiations.3l";
+    public static string demoTextFilePath = @"../../../../Samples/ism2023_instances.3l";
     //public static string demoTextFilePath = @"../../../packages/ai.hgb.packages.base/ai.hgb.packages.base.index.3l";
     //public static string demoTextFilePath = @"../../../packages/ai.hgb.packages.base/ai.hgb.packages.base.network.3l";
     //public static string demoTextFilePath = @"../../../packages/ai.hgb.packages.base/ai.hgb.packages.base.utils.3l";
     //public static string demoTextFilePath = @"../../../packages/ai.hgb.packages.demoapps/ai.hgb.packages.demoapps.procon.3l";
     //public static string demoTextFilePath = @"G:\My Drive\FHHAGENBERG\FE\Publications\2024_Eurocast\Presentation\Samples\eurocast2024_instances.3l";
-    public static string demoTextFilePath = @"G:\My Drive\FHHAGENBERG\FE\Publications\2024_Eurocast\Presentation\Samples\eurocast2024.3l";
+    //public static string demoTextFilePath = @"G:\My Drive\FHHAGENBERG\FE\Publications\2024_Eurocast\Presentation\Samples\eurocast2024.3l";
 
 
     public static string repositoryHost = "127.0.0.1";
